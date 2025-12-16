d7e0690de0c5bebe7b6d848746ed352f90447b89
DBKAHHK <abc88435832@gmail.com>
2025-12-15 21:32:09 -0800

    Add files via upload

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -41,7 +41,8 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.documentfile:documentfile:1.0.1")
     implementation("com.google.android.material:material:1.12.0")
 }
-
